1afeef531419f6fca75a7aab63e7f0f083520859
strahser <67167650+strahser@users.noreply.github.com>
2023-08-10 12:07:19 +0300

    dash

--- DashBoard/DashBoardMain_.py
+++ DashBoard/DashBoardMain_.py
@@ -8,10 +8,12 @@ from Session.StatementConfig import StatementConstants
 from StaticData.AppConfig import MenuChapters
 from InputView.NodesView import ViewNodes
 import pygwalker as pyg
-import numpy as np
+import codecs
+from ydata_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
 # pip install https://github.com/pandas-profiling/pandas-profiling/archive/master.zip
-
+import sweetviz as sv
+import streamlit.components.v1 as components
 
 
 from SQL.SqlModel.SqlConnector import SqlConnector
@@ -81,12 +83,16 @@ def dashboard_main(upload_layout):
 
 	with pd_profile:
 		table_name = _create_input_sql_tools_view(upload_layout, f"{MenuChapters.dash_board} profile pandas")
-		df = pd.read_sql(f"select * from {table_name}",
-		                 con=SqlConnector.conn_sql)
-		show_report_button = st.button("Show Report",key="show report button")
-		if show_report_button:
-			pr = df.profile_report()
-			st_profile_report(pr)
+		if table_name:
+			df = pd.read_sql(f"select * from {table_name}",
+			                 con=SqlConnector.conn_sql)
+			try:
+				show_report_button = st.button("Show Report",key="show report button")
+				if show_report_button:
+					pr = ProfileReport(df)
+					st_profile_report(pr)
+			except Exception as e:
+				st.warning(e)
 
 
 def _check_is_table_name_in_list_all_tables_all_views(table: str) -> bool:
